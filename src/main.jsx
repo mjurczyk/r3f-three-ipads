c6be1a57e77d4e55c28e2d23b4636b261c0020c8
mjurczyk <thelairmail@gmail.com>
2024-08-08 10:00:59 +0700

    Test

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { R3fPreview } from './R3fPreview';
+// import { StrictMode } from 'react';
+// import { createRoot } from 'react-dom/client';
+// import { R3fPreview } from './R3fPreview';
 import './styles.css';
 import './three';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <R3fPreview />
-  </StrictMode>,
-);
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <R3fPreview />
+//   </StrictMode>,
+// );
